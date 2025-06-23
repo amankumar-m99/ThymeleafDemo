447a530d4f0a9f8bc61fdefca747aed512d1f0ba
Aman Kumar <amankumar.m99@gmail.com>
2025-06-23 22:41:15 +0530

    updated thymeleaf

---
+++ src/main/java/indeedcoder/thymeleafdemo/controller/HomeController.java
@@ -0,0 +1,31 @@
+package indeedcoder.thymeleafdemo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import indeedcoder.thymeleafdemo.model.Employee;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/home")
+	public String getHome() {
+		return "HomePage";
+	}
+
+	@GetMapping("/data")
+	public String getData(Model m) {
+		List<Employee> employees = new ArrayList<Employee>();
+		employees.add(new Employee("ABC", 30, 12000));
+		employees.add(new Employee("MNO", 32, 18000));
+		employees.add(new Employee("PQR", 40, 32000));
+		employees.add(new Employee("XYZ", 43, 52000));
+		m.addAttribute("preparedBy", "Aman");
+		m.addAttribute("employees", employees);
+		return "DataPage";
+	}
+}
---
+++ src/main/java/indeedcoder/thymeleafdemo/model/Employee.java
@@ -0,0 +1,15 @@
+package indeedcoder.thymeleafdemo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Employee {
+
+	public String name;
+	public int age;
+	public double salary;
+}
